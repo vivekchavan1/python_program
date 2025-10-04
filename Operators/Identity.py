9d13ee2bf240aa7af2ed36b5e4e182ff8898d3a1
vivekchavan1 <chavanvivek7722@gmail.com>
2025-10-04 12:45:15 +0000

    5/10

---
+++ Operators/Identity.py
@@ -0,0 +1,5 @@
+a = ["Virat","Icon"]
+b = ["Virat","Icon"]
+c = a
+print("a is c ", a is c)    
+print("a is not c", a is not c)
---
+++ Variables/Numeric.py
@@ -0,0 +1,25 @@
+# var_1=20
+# var_2=30
+# var_3=25
+# print(var_1)
+# print(var_2)
+# print(var_3)
+
+# print(type(var_1))
+
+# var_1=var_2=var_3=35
+# print("variable:",var_1)
+# print("variable:",var_2)
+# print("variable:",var_3)
+
+# var_1,var_2,var_3=35,"icon",3.18
+# print("variable_1:",var_1)
+# print("variable_2:",var_2)
+# print("variable_3:",var_3)
+var_1="icon"
+var_2=30
+var_3=3+4j
+print("Data Type of", var_1, "=", type(var_1))  
+print("Data Type of", var_2, "=", type(var_2))  
+print("Data Type of", var_3, "=", type(var_3)) 
+

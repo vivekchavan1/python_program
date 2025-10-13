8fdc4c2d86afb55421a6dc80cacf3f3973a664e3
vivekchavan1 <chavanvivek7722@gmail.com>
2025-10-13 12:24:59 +0000

    13/10

---
+++ Control Statement/if-else statement.py
@@ -0,0 +1,66 @@
+# marks = 35    
+# if marks >= 90:
+#     print("A+")
+
+# elif marks >= 80:
+#     print("A")
+
+# elif marks >= 70:
+#     print("B")
+
+# elif marks >= 60:
+#     print("C")
+
+# elif marks >= 50:
+#     print("D")
+
+# elif marks >= 35:
+#     print("pass")
+
+# else:
+#     print("failed")   
+  
+#positive number
+# num = 12
+# if num > 0:
+#     print("the number is positive")
+
+# elif num != 0:
+#     print("the number is positive")
+
+# else:
+#     print("negative")   
+
+
+#Even and Odd Number
+
+# num = 10
+# if num % 2 == 0:
+#     print("Even")
+
+# else:
+#     print("Odd")         
+
+
+#Greater number among 2 number
+
+# num1 = 20
+# num2 = 15
+# if num1 > num2:
+#     print("Greater")
+
+# else:
+#     print("smaller")  
+
+#check if the year is leaf year in single condition
+y = 2000
+if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+    print("Leap year") 
+else:
+    print("Not a leap year")      
+
+
+
+ 
+
+

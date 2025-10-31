058e6884826ce19adc2b861a781b5745512c2c32
vivekchavan1 <chavanvivek7722@gmail.com>
2025-10-31 12:34:34 +0000

    31/10

---
+++ Control Statement/Loop/while-loop.py
@@ -0,0 +1,19 @@
+# counter = 0
+# while counter < 5:
+#     print(counter, "Hello")
+#     counter += 1
+
+# i = 1
+# while i < 50:
+#     if i % 5 == 0 or i % 7 == 0:
+#         print(i,end=' ')
+#     i += 1
+
+
+number = 1
+sum = 0
+
+while number < 50:
+    sum = number/2 + sum
+    number += 1
+print(sum)    
---
+++ python data structure/first.py
@@ -0,0 +1,42 @@
+# List = [1,2,3,4,5]
+# print(List)
+
+
+#multiple value
+
+# List = [1,3,"icon",3.14]
+# # print(List)
+# print(List(type))
+
+#mutable list(changeable)
+# List_1 = [23,25, 16.25, 'TpointTech', [20, 40, 60], 3e8]
+# List_1[2] = "Welcome"
+# print(List_1) 
+
+#Creating a List
+
+# Empty_list = []
+# Number = [2,45,23,56,23]
+# Mixed_list = [3,'icon,3.14']
+
+# print(Empty_list)
+# print(Number)
+# print(Mixed_list)
+
+#Accessing List
+# My_list = [24,24,74,12,35]
+
+# print(My_list[2])
+# print(My_list[-2])
+
+#Adding Elements in list
+
+# my_list = [23,45,"icon"]
+# my_list.append(5)
+# print(my_list)
+# my_list.insert(1,23)
+# print(my_list)
+# my_list.extend([34,23,12])
+# print(my_list)
+               
+               

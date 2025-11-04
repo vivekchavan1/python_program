b72a4fbd55dae21dd66fe3553768cba984b7265d
vivekchavan1 <chavanvivek7722@gmail.com>
2025-11-04 12:32:06 +0000

    4/11

---
+++ python data structure/Tuple.py
@@ -0,0 +1,44 @@
+#Accessing Tuble
+
+# sampleTuple = ("Apple","Banana","Mango","Grapes","Orange")
+
+# print("sampleTuple[0]=",sampleTuple[0])
+# print("sampleTuple[1]=",sampleTuple[1])
+# print("sampleTuple[2]=",sampleTuple[2])
+# print("sampleTuple[3]=",sampleTuple[3])
+# print("sampleTuple[-3]=",sampleTuple[-3])
+
+
+#Slicing the Tuples
+
+# sampleTuple = ("Icon","Chidku","Roshan","Mayur","Anu")
+
+# print("Elements between indices 0 to 2:",sampleTuple[0:2])
+
+# print("Elements betweeen indices 1 to 4:",sampleTuple[1:4])
+
+
+#Changing the Elements in Tuple
+
+# fruits_tuple = ("mango", "orange", "banana", "apple", "papaya")
+
+# print("Before Changing the Element in Tuple...")  
+# print("Tuple =", fruits_tuple)
+
+# fruits_list = list(fruits_tuple)
+
+# fruits_list[2] = "grapes"  
+# print("Converting", fruits_tuple[2], "=>", fruits_list[2])
+
+# fruits_tuple = tuple(fruits_list) 
+
+# print("After Changing the Element in Tuple...")  
+# print("Tuple =", fruits_tuple)
+
+
+#Converting the Tuple into a List
+
+
+
+
+

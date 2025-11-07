2b79ea06bbf1a3fb432dd16edadfe5c7c62f6f92
vivekchavan1 <chavanvivek7722@gmail.com>
2025-11-07 12:26:23 +0000

    7/11

---
+++ python data structure/Set.py
@@ -0,0 +1,51 @@
+# S = {"Icon","Anu","Ashu","Chidku","Anjira"}
+# print(S)
+
+# S.add("Chiuu")
+# print(S)
+
+# S.update(["KING","Run Machine"])
+# print(S)
+
+# S.remove("Ashu")
+# print(S)
+
+# S.pop()
+# print(S)
+
+
+#Set Opertions
+
+# Set_A = {1,2,3,4}
+# print("Set_A:",Set_A)
+
+# Set_B = {2,5,6,7,8}
+# print("Set_B:",Set_B)
+
+# #Union of Set A and B
+
+# print(Set_A | Set_B)
+
+# #Intersection of set A and B
+
+# print(Set_A & Set_B)
+
+# #Common from both set
+
+# print(Set_A - Set_B)
+
+# print(Set_B - Set_A)
+
+
+#Copy set
+
+# Vegetable_set = {"potato","brinjal","tomato","cabbage"}
+# print("Vegetable_set:",Vegetable_set)
+
+# Dummy_set = Vegetable_set.copy()
+# print("Dummy_set:",Dummy_set)
+
+#Set Compression
+
+Set_1 = {i**4 for i in range(5)}
+print("Set_1:",Set_1)

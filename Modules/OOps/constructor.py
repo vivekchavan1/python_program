8297bde3c8e8b893065a5336ad12118f2425a30d
vivekchavan1 <chavanvivek7722@gmail.com>
2025-12-01 12:30:51 +0000

    1/12

--- Modules/OOps/constructor.py
+++ Modules/OOps/constructor.py
@@ -1,3 +1,5 @@
+#creating constructor in pythyon
+
 class Employee:
     def __init__ (self,name,id):
         self.id = id
---
+++ Modules/OOps/inheritance.py
@@ -0,0 +1,76 @@
+# class Animal:
+#     def speak(self):
+#         print("Animal Speaking")
+
+# class Dog(Animal):
+#     def bark(self):
+#         print("Dog Barking")
+
+# d = Dog()
+# d.bark()
+# d.speak()
+
+# class Girl:
+#     def purpose(self):
+#         print("Girl Purpose")
+
+# class Roshan(Girl):
+#     def reject(self):
+#         print("Roshan Reject")
+
+# r = Roshan()
+# r.reject()
+# r.purpose()               
+
+
+#Multiple Level Inheritance
+
+# class Animal:  
+#     def speak(self):  
+#         print("Animal Speaking")  
+  
+# class Dog(Animal):  
+#     def bark(self):  
+#         print("dog barking")  
+ 
+# class DogChild(Dog):  
+#     def eat(self):  
+#         print("Eating bread...")  
+# d = DogChild()  
+# d.bark()  
+# d.speak()  
+# d.eat()
+# d.eat()  
+
+
+#Multiple Inheritance
+
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(d.Summation(10,20))  
+print(d.Multiplication(10,20))  
+print(d.Divide(10,20))
+
+
+#The issubclass(sub,sup)
+
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(issubclass(Derived,Calculation2))  
+print(issubclass(Calculation1,Calculation2))  
